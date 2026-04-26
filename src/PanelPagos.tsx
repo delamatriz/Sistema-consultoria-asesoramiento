379d3e900ea2349a231cf7cbee4180b186611e60
delamatriz <delamatriz@gmail.com>
2026-04-26 11:10:28 -0300

    Suavizar negros en PanelPagos a gris medio
    Suavizar negros en PanelPagos a gris medio

--- src/PanelPagos.tsx
+++ src/PanelPagos.tsx
@@ -4,7 +4,7 @@ import { db } from './firebase/config';
 
 const THEME = {
   primary: '#B21F24',
-  text: '#1D1D1F',
+  text: '#6E6E73',
   background: '#F5F5F7',
   white: '#FFFFFF',
   gray: '#86868B',
